d1ef876894dbbe69eb97cc8616e29871191350c4
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-12 16:39:11 -0400

    added models

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+    reactionID: {
+        _id: mongoose.Types.ObjectId, 
+    },
+    reactionBody: {
+        type: String,
+        required: true, 
+        maxLength: 280
+    }, 
+    username: {
+        type: String, 
+        required: true
+    },
+    createdAt: {
+        type: Date, 
+        default: Date.now, 
+        get: (date) => {
+            if (date) return date.toISOString().split("T")[0];
+        },
+    }
+});
+
+module.exports = reactionSchema;
